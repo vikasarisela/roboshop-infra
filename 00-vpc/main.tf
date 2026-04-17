734a4152f0429b4e4b9a055f48ecd62eb8d55399
unknown <vikas.arisela@gmail.com>
2026-04-17 16:33:50 +0400

    Initial commit

---
+++ 00-vpc/main.tf
@@ -0,0 +1,38 @@
+module "vpc" {
+  source = "git::https://github.com/vikasarisela/terraform-vpc.git?ref=main"
+  # cidr_block = "10.0.0.0/16"
+  # project_name = "Roboshop"
+  # environment = "dev"
+  # public_subnet_cidr =   ["10.0.1.0/24","10.0.2.0/24"]
+  # private_subnet_cidr =  ["10.0.11.0/24","10.0.12.0/24"]
+  # database_subnet_cidr = ["10.0.21.0/24","10.0.22.0/24"]
+
+//  is_peering_required = false
+
+
+# VPC
+    vpc_cidr = var.vpc_cidr
+    project_name = var.project_name
+    environment = var.environment
+    vpc_tags = var.vpc_tags
+
+    # public subnets
+    public_subnet_cidrs = var.public_subnet_cidrs
+
+    # private subnets
+    private_subnet_cidrs = var.private_subnet_cidrs
+
+    # database subnets
+    database_subnet_cidrs = var.database_subnet_cidrs
+
+    is_peering_required = true
+}
+
+# output "azs" {
+#   value = data.aws_availability_zones.available.names
+# }
+
+
+
+# terraform init -upgrade 
+# to get the latest from module 
---
+++ 00-vpc/outputs.tf
@@ -0,0 +1,12 @@
+output "public_subnet_idss" {
+  value = module.vpc.public_subnet_ids
+}
+
+
+output "private_subnet_idss" {
+  value = module.vpc.private_subnet_ids
+}
+
+output "database_subnet_idss" {
+  value = module.vpc.database_subnet_ids
+}
