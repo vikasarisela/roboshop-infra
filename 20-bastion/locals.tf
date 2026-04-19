eba2f2db5ad29254e2d3d8c92d8ebb4ed3652250
unknown <vikas.arisela@gmail.com>
2026-04-19 17:11:23 +0400

    bastion

--- 20-bastion/locals.tf
+++ 20-bastion/locals.tf
@@ -1,6 +1,8 @@
 locals {
   ami_id = data.aws_ami.amidata.id
   bastion_sg_id = data.aws_ssm_parameter.bastion_sg_id.value
+  public_subnet_ids = split("," , data.aws_ssm_parameter.public_subnet_ids.value)[0]
+
   common_tags = {
     Project = var.project_name
     Environment = var.environment
--- 20-bastion/main.tf
+++ 20-bastion/main.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "bastion" {
     ami = local.ami_id
-
     instance_type = "t3.micro"
     vpc_security_group_ids = [local.bastion_sg_id]
+    subnet_id = local.public_subnet_ids
     tags = merge(
         local.common_tags,
         {
